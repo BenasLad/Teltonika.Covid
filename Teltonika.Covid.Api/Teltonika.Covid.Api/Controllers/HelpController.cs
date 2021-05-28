adda95375fcf83086e9be3965bce4b8d9e4e2159
unknown <benas.ladzikas@gmail.com>
2021-05-28 18:21:08 +0300

    Initial project

---
+++ Teltonika.Covid.Api/Teltonika.Covid.Api/Controllers/HelpController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teltonika.Covid.Api.Controllers
+{
+    public class HelpController
+    {
+    }
+}
